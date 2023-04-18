d1fc17b554d49db9ab3e29daa765bf8fe24b36b9
BennyC31 <bencalderaio@gmail.com>
2023-04-18 16:29:18 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 import setuptools
 
+with open('README.md','r') as rfile:
+        long_description = rfile.read()
 setuptools.setup(
+    
     include_package_data=True,
     name='py-panda-types',
-    version='0.0.1',
+    version='0.0.2',
     description='Package for bundling data files into one Data Frame.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['pandas==2.0.0'],
-    url='git@github.com:BennyC31/py-panda-types.git',
+    url='https://github.com/BennyC31/py-panda-types.git',
     author='Benjamin Calderaio, Jr.',
     author_email='bencalderaio@gmail.com',
     packages=['pypantypes'],
     license='MIT',
     classifiers=[
-        'Programming Language :: Python :: 3.9.0',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent',
     ],
 )
